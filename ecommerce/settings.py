41a9e273a9491eb89627d82c728c4321d21950b9
dalienst <oduordalienst@gmail.com>
2023-06-09 01:33:19 +0300

    remove static files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -160,8 +160,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
